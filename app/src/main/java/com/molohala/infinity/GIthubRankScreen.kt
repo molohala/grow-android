0859e67c4c769ea90c571570ea83335fea101737
bestswlkh0310 <hhhello0507@gmail.com>
2024-04-03 15:42:47 +0900

    feat : create github rank cell

--- app/src/main/java/com/molohala/infinity/GIthubRankScreen.kt
+++ app/src/main/java/com/molohala/infinity/GIthubRankScreen.kt
@@ -1,9 +1,19 @@
 package com.molohala.infinity
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -15,19 +25,60 @@ fun GithubRankScreen(
     navController: NavController,
     viewModel: GithubRankViewModel = viewModel()
 ) {
-    Column(
-        modifier = Modifier
-            .background(Color.White)
-    ) {
-        Title(
-            modifier = Modifier
-                .padding(horizontal = 16.dp),
-            text = "Github 랭킹"
-        )
-        GithubRankCell(
-            rank = 1
-        ) {
 
+    val tempRankings = Array(10) { it }
+
+    LazyColumn(
+        modifier = Modifier
+            .background(Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        item {
+            Title(
+                modifier = Modifier
+                    .padding(horizontal = 16.dp),
+                text = "Github 랭킹"
+            )
+            Column(
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .border(
+                        width = 1.5.dp,
+                        color = Color.LightGray.copy(alpha = 0.5f),
+                        shape = RoundedCornerShape(10)
+                    )
+                    .padding(horizontal = 20.dp, vertical = 24.dp)
+            ) {
+                Text(
+                    modifier = Modifier,
+                    text = "아직 Github 설정이 완료되지 않았어요",
+                    color = Color.Gray,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Text(
+                    text = "Github Id를 설정하고 순위권 도전해 보세요!",
+                    style = MaterialTheme.typography.titleMedium,
+                    color = Color.Black
+                )
+                InfinityButton(
+                    modifier = Modifier
+                        .width(110.dp)
+                        .height(40.dp),
+                    text = "설정하기"
+                ) {
+                    // navigate to profile edit view
+                }
+            }
+        }
+        items(tempRankings) {
+            GithubRankCell(
+                modifier = Modifier
+                    .padding(horizontal = 20.dp),
+                rank = it + 1
+            ) {
+
+            }
         }
     }
 }
--- app/src/main/java/com/molohala/infinity/GithubRankCell.kt
+++ app/src/main/java/com/molohala/infinity/GithubRankCell.kt
@@ -1,28 +1,40 @@
 package com.molohala.infinity
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun GithubRankCell(
+    modifier: Modifier = Modifier,
     rank: Int,
     onClick: () -> Unit
 ) {
     Row(
-        modifier = Modifier
-            .bounceClick(onClick = onClick),
-        horizontalArrangement = Arrangement.spacedBy(12.dp)
+        modifier = modifier
+            .bounceClick(onClick = onClick)
+            .defaultMinSize(minHeight = 60.dp),
+        horizontalArrangement = Arrangement.spacedBy(12.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
             text = "$rank",
@@ -30,9 +42,53 @@ fun GithubRankCell(
             color = Color.Black
         )
         Box(
+            contentAlignment = Alignment.TopCenter
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(CircleShape)
+                    .background(Color.LightGray),
+            )
+            val medal = when (rank) {
+                1 -> R.drawable.first_medal
+                2 -> R.drawable.second_medal
+                3 -> R.drawable.third_medal
+                else -> null
+            }
+            if (rank in 1..<4 && medal != null) {
+                Column {
+                    Image(
+                        modifier = Modifier
+                            .size(28.dp),
+                        painter = painterResource(id = medal),
+                        contentDescription = null
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+                }
+            }
+        }
+        Column(
+            verticalArrangement = Arrangement.spacedBy(0.dp)
+        ) {
+            Text(
+                text = "노영재",
+                color = Color.Black,
+                style = MaterialTheme.typography.titleMedium.copy(fontSize = 16.sp)
+            )
+            Text(
+                text = "nohjason",
+                color = Color.Gray,
+                style = MaterialTheme.typography.titleMedium
+            )
+        }
+        Spacer(
             modifier = Modifier
-                .background(Color.LightGray)
-                .clip(CircleShape)
+                .weight(1f)
+        )
+        Text(
+            text = "10 커밋",
+            fontWeight = FontWeight.SemiBold
         )
     }
 }
