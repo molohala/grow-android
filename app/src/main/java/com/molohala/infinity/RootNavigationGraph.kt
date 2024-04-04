fb603a31f6da73076c86ac57c18b7ca8be93915e
bestswlkh0310 <hhhello0507@gmail.com>
2024-04-04 09:35:59 +0900

    feat : create bottom navigation

--- app/src/main/java/com/molohala/infinity/RootNavigationGraph.kt
+++ app/src/main/java/com/molohala/infinity/RootNavigationGraph.kt
@@ -1,7 +1,6 @@
 package com.molohala.infinity
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -17,7 +16,7 @@ fun RootNavigationGraph(
             SignInScreen()
         }
         composable("main") {
-            MainNavigationGraph()
+            MainScreen()
         }
     }
 }
