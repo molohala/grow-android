73dd189586307aa139cc143e06f1271d6fd4a2e3
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-05-03 23:43:58 +0900

    feat : create GrowDivider

---
+++ presentation/src/main/java/com/molohala/grow/designsystem/component/divider/GrowDivider.kt
@@ -0,0 +1,51 @@
+package com.molohala.grow.designsystem.component.divider
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.molohala.grow.designsystem.foundation.GrowTheme
+import com.molohala.grow.designsystem.foundation.util.GrowPreviews
+
+sealed class GrowDividerThickness(val size: Dp) {
+    data object Thin: GrowDividerThickness(size = 1.dp)
+    data object Thick: GrowDividerThickness(size = 8.dp)
+}
+
+@Composable
+fun GrowDivider(
+    modifier: Modifier = Modifier,
+    thickness: GrowDividerThickness = GrowDividerThickness.Thin,
+    color: Color = GrowTheme.colorScheme.dividerNormal
+) {
+    Box(
+        modifier = modifier
+            .background(color)
+            .fillMaxWidth()
+            .height(thickness.size),
+    )
+}
+
+@Composable
+@GrowPreviews
+private fun Preview() {
+    GrowTheme {
+        Column(
+            modifier = Modifier
+                .background(GrowTheme.colorScheme.background)
+                .padding(20.dp),
+            verticalArrangement = Arrangement.spacedBy(32.dp)
+        ) {
+            GrowDivider()
+            GrowDivider(thickness = GrowDividerThickness.Thick)
+        }
+    }
+}
