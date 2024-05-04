61ffc8b9e2ccaf0bfe8abe58a16e33cb1a5a2485
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-05-04 12:51:31 +0900

    feat : create SettingCell

--- presentation/src/main/java/com/molohala/grow/ui/main/profile/setting/SettingScreen.kt
+++ presentation/src/main/java/com/molohala/grow/ui/main/profile/setting/SettingScreen.kt
@@ -7,15 +7,14 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.molohala.grow.R
 import com.molohala.grow.designsystem.color.GrowColor
 import com.molohala.grow.designsystem.component.topappbar.GrowTopAppBar
+import com.molohala.grow.designsystem.specific.settingcell.GrowSettingCell
 import com.molohala.grow.ui.main.main.NavGroup
 import com.molohala.grow.ui.main.profile.Logout
-import com.molohala.grow.ui.main.profile.setting.component.SettingCell
 import com.molohala.grow.ui.root.AppViewModel
 
 
@@ -37,27 +36,24 @@ fun SettingScreen(
                 .padding(horizontal = 16.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            SettingCell(
+            GrowSettingCell(
                 modifier = Modifier
                     .padding(top = 16.dp),
-                icon = R.drawable.ic_person,
-                iconColor = Color.Gray,
-                text = "프로필 설정"
+                leftIcon = R.drawable.ic_person,
+                label = "프로필 설정"
             ) {
                 navController.navigate(NavGroup.ProfileEdit.name)
             }
-            SettingCell(
-                icon = R.drawable.ic_github,
-                iconColor = GrowColor.github,
-                text = "Github 설정",
+            GrowSettingCell(
+                leftIcon = R.drawable.ic_github,
+                label = "Github 설정",
                 description = "bestswlkh0310"
             ) {
                 navController.navigate(NavGroup.GithubSetting.name)
             }
-            SettingCell(
-                icon = R.drawable.ic_baekjoon,
-                iconColor = GrowColor.baekjoon,
-                text = "백준 설정",
+            GrowSettingCell(
+                leftIcon = R.drawable.ic_baekjoon,
+                label = "백준 설정",
                 description = "hhhello0507"
             ) {
                 navController.navigate(NavGroup.BaekjoonSetting.name)
