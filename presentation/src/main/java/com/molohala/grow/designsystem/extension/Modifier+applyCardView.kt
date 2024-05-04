fe6817600526d1b4b5419218e12852c38dcb7f4f
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-05-04 11:30:41 +0900

    fix : apply card view

--- presentation/src/main/java/com/molohala/grow/designsystem/extension/Modifier+applyCardView.kt
+++ presentation/src/main/java/com/molohala/grow/designsystem/extension/Modifier+applyCardView.kt
@@ -1,24 +1,17 @@
 package com.molohala.grow.designsystem.extension
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.molohala.grow.designsystem.extension.shadow
+import com.molohala.grow.designsystem.foundation.GrowTheme
+import com.molohala.grow.designsystem.foundation.shadow.ShadowType
+import com.molohala.grow.designsystem.foundation.shadow.growShadow
 
 @Composable
-fun Modifier.applyCardView(): Modifier = this
-    .shadow(
-        color = Color.Black,
-        alpha = 0.04f,
-        offsetY = 3.dp,
-        blur = 8.dp,
-    )
-    .clip(RoundedCornerShape(10.dp))
-    .background(Color.White)
-    .padding(all = 12.dp)
+fun Modifier.applyCardView() = this
+    .clip(RoundedCornerShape(12.dp))
+    .growShadow(ShadowType.ElevationBlack1)
+    .background(GrowTheme.colorScheme.background)
