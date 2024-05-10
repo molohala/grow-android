c74ea04d8cdb9c73640ae31807e96ef120ecc7fa
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-05-10 10:39:54 +0900

    feat : DAuthSignIn

--- presentation/src/main/java/com/molohala/grow/data/dauth/response/DAuthSignInResponse.kt
+++ presentation/src/main/java/com/molohala/grow/data/dauth/response/DAuthSignInResponse.kt
@@ -2,6 +2,6 @@ package com.molohala.grow.data.dauth.response
 
 data class DAuthSignInResponse(
     public val name: String,
-    public val profileImage: String,
+    public val profileImage: String?,
     public val location: String
 )
