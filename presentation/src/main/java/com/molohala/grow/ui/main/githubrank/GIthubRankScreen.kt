a58d4491c3956688ce82facc784edc718bd40652
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-05-12 09:11:32 +0900

    fix : RankScreen 뷰 로직 변경

--- presentation/src/main/java/com/molohala/grow/ui/main/githubrank/GIthubRankScreen.kt
+++ presentation/src/main/java/com/molohala/grow/ui/main/githubrank/GIthubRankScreen.kt
@@ -1,6 +1,5 @@
 package com.molohala.grow.ui.main.githubrank
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,14 +7,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
@@ -76,51 +73,52 @@ fun GithubRankScreen(
                     RecommendingSettingGithub(onClickSetting = {
                         navController.navigate(NavGroup.GithubSetting.name)
                     })
-                }
-                Indicator(
-                    selectedTab = uiState.selectedTab,
-                    onClickTab = {
-                        viewModel.clickTab(selectedTab = it)
-                    }
-                )
-                uiState.githubRanksFetchFlow.let {
-                    when (it) {
-                        is FetchFlow.Failure -> {
-                            ErrorScreen(
-                                modifier = Modifier
-                                    .fillMaxSize()
-                            )
+                } else {
+                    Indicator(
+                        selectedTab = uiState.selectedTab,
+                        onClickTab = {
+                            viewModel.clickTab(selectedTab = it)
                         }
-
-                        is FetchFlow.Fetching -> {
-                            Column(
-                                modifier = Modifier.padding(12.dp)
-                            ) {
-                                repeat(4) {
-                                    GrowRankCellShimmer()
-                                }
+                    )
+                    uiState.githubRanksFetchFlow.let {
+                        when (it) {
+                            is FetchFlow.Failure -> {
+                                ErrorScreen(
+                                    modifier = Modifier
+                                        .fillMaxSize()
+                                )
                             }
-                        }
 
-                        is FetchFlow.Success -> {
-                            LazyColumn(
-                                modifier = Modifier.padding(12.dp),
-                                horizontalAlignment = Alignment.CenterHorizontally,
-                                verticalArrangement = Arrangement.spacedBy(12.dp),
-                                state = scrollState
-                            ) {
-                                items(it.data) { rank ->
-                                    GrowRankCell(
-                                        name = rank.memberName,
-                                        socialId = rank.socialId,
-                                        rank = rank.rank,
-                                        label = "${rank.count} 커밋"
-                                    ) {
-                                        navController.navigate("${NavGroup.ProfileDetail.name}/${rank.memberId}")
+                            is FetchFlow.Fetching -> {
+                                Column(
+                                    modifier = Modifier.padding(12.dp)
+                                ) {
+                                    repeat(4) {
+                                        GrowRankCellShimmer()
                                     }
                                 }
-                                item {
-                                    Spacer(modifier = Modifier.height(64.dp))
+                            }
+
+                            is FetchFlow.Success -> {
+                                LazyColumn(
+                                    modifier = Modifier.padding(12.dp),
+                                    horizontalAlignment = Alignment.CenterHorizontally,
+                                    verticalArrangement = Arrangement.spacedBy(12.dp),
+                                    state = scrollState
+                                ) {
+                                    items(it.data) { rank ->
+                                        GrowRankCell(
+                                            name = rank.memberName,
+                                            socialId = rank.socialId,
+                                            rank = rank.rank,
+                                            label = "${rank.count} 커밋"
+                                        ) {
+                                            navController.navigate("${NavGroup.ProfileDetail.name}/${rank.memberId}")
+                                        }
+                                    }
+                                    item {
+                                        Spacer(modifier = Modifier.height(64.dp))
+                                    }
                                 }
                             }
                         }
@@ -141,17 +139,11 @@ private fun RecommendingSettingGithub(
 ) {
     Column(
         modifier = Modifier
-            .padding(horizontal = 12.dp, vertical = 16.dp)
-            .fillMaxWidth()
-            .border(
-                width = 1.dp,
-                color = GrowTheme.colorScheme.textAlt,
-                shape = RoundedCornerShape(12)
-            )
-            .padding(16.dp),
+            .fillMaxSize(),
         verticalArrangement = Arrangement.spacedBy(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+        Spacer(modifier = Modifier.weight(1f))
         Text(
             modifier = Modifier,
             text = "아직 Github ID를 설정하지 않았어요",
@@ -164,6 +156,7 @@ private fun RecommendingSettingGithub(
         ) {
             onClickSetting()
         }
+        Spacer(modifier = Modifier.weight(1f))
     }
 }
 
